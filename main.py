4fd2a3bd29b2dadcfc93eb8907ced4ad8181d09d
karolinashere <karolinastec1998@wp.pl>
2025-07-09 18:53:00 +0200

    cron implementation

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from api import fetch_weather
 # import pandas
 # import openpyxl
 from excel import append_to_excel
+import time
 data = fetch_weather()
 
 append_to_excel(data)
+
+while True:
+    data = fetch_weather()
+
+    append_to_excel(data)
+
+    time.sleep(45)
+    print("Pobrano nowe dane")
+#CRON
